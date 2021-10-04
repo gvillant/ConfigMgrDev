691ea0f034aaa7f02bab0699b4a96d4be55a0d7f
Gaetan <32365616+gvillant@users.noreply.github.com>
2021-10-04 23:43:37 +0200

    Add files via upload

---
+++ _Intune/Install-ChocolateyPackages.ps1
@@ -0,0 +1,24 @@
+﻿#From https://www.petervanderwoude.nl/post/combining-the-powers-of-the-intune-management-extension-and-chocolatey/
+
+$ChocoPackages = @(“googlechrome”,”adobereader”,”notepadplusplus.install”,”7zip.install”)
+$ChocoInstall = Join-Path ([System.Environment]::GetFolderPath(“CommonApplicationData”)) “Chocolatey\bin\choco.exe”
+
+#install Chocolatey if not already there.
+if(!(Test-Path $ChocoInstall)) {
+     try {
+         Invoke-Expression ((New-Object net.webclient).DownloadString(‘https://chocolatey.org/install.ps1’)) -ErrorAction Stop
+     }
+     catch {
+         Throw “Failed to install Chocolatey”
+     }       
+}
+
+#install ChocoPackages ...
+foreach($Package in $ChocoPackages) {
+     try {
+         Invoke-Expression “cmd.exe /c $ChocoInstall Install $Package -y” -ErrorAction Stop
+     }
+     catch {
+         Throw “Failed to install $Package”
+     }
+}
